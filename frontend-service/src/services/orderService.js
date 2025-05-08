0c7a6903c0126e415a7790ead36b7f1b5b20c944
prithvi <prithvi8754@gmail.com>
2025-05-07 20:33:59 -0700

    Modern food ordering flow: restaurant and menu selection on Dashboa3

--- frontend-service/src/services/orderService.js
+++ frontend-service/src/services/orderService.js
@@ -1,10 +1,10 @@
 export const createOrder = (data) =>
-  fetch(`${process.env.REACT_APP_ORDER_API_URL}/orders`, {
+  fetch(`http://a666dd10fea464039b8cfacf6c27bb38-1881340992.us-east-1.elb.amazonaws.com:8181/orders`, {
     method: 'POST',
     headers: { 'Content-Type': 'application/json' },
     body: JSON.stringify(data),
   }).then(res => res.json());
 
 export const getOrder = (trackingId) =>
-  fetch(`${process.env.REACT_APP_ORDER_API_URL}/orders/${trackingId}`)
+  fetch(`http://a666dd10fea464039b8cfacf6c27bb38-1881340992.us-east-1.elb.amazonaws.com:8181/orders/${trackingId}`)
     .then(res => res.json());
